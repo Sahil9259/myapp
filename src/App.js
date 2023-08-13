682ae6f1f14359158e23be91efcba6614286e00a
Sahil Singh <200104050@hbtu.ac.in>
2023-08-13 18:46:50 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Condition from './pages/Condition';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/condition" element={<Condition />} />
+            </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Condition.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+export default function Condition() {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div className="container">
+        Term and Conditions
+      </div>
+    </>
+  );
+}
