2be4a5285a51dd10eb75b4693f8b72bece7c0dce
Sahil Singh <200104050@hbtu.ac.in>
2023-08-24 16:27:55 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatapp-sahil9259.netlify.app",
     credentials: true,
   },
 });
